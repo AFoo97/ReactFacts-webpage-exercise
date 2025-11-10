0a7880a9659a0a5040a5e3f8543b119428ff921a
AFoo97 <arthur.foozb@gmail.com>
2025-11-10 11:27:04 +1100

    ReactFacts webpage v1

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Main from './components/Main.jsx'
+import Navbar from './components/Navbar.jsx'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Main />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+export default function Navbar() {
+  return (
+    <header>  
+      <nav>
+        <img src="./react-logo.png" alt="React logo"/> 
+        <span>React Facts</span>
+        
+      </nav>
+    </header>
+    
+  )
+}
